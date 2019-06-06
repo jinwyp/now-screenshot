fadf8db8e5877612e6746df27b56ee4e8f13e877
wangyupeng <wangyp@zenmen.com>
2019-06-06 10:44:48 +0800

    U-update

--- backend/lib/shot.js
+++ backend/lib/shot.js
@@ -52,7 +52,7 @@ async function screenShot (url) {
         return await page.evaluate( () => {
             return new Promise((resolve, reject) => {
                 let totalHeight = 0
-                const distance = 500
+                const distance = 400
                 const maxHeight = 10000
 
                 let timeInterval = 300
